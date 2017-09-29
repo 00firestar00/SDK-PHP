2c07254adc8ac790c234d8e83320c807b5d9881d
Evan Jarrett <ejfirestar00@gmail.com>
2017-09-29 11:17:38 -0700

    will this work in 5.4?

--- tests/ContactsTest.php
+++ tests/ContactsTest.php
@@ -9,13 +9,25 @@ class ContactsTest extends TestCase
 
     public function testRetrieveSingle()
     {
-        // Create a stub for the SomeClass class.
-        $stub = $this->createMock(CurlClient::class);
 
-        // Configure the stub.
-        $stub->method('httpRequest')
-             ->willReturn('{"code":0, "data":{"id":"1", "firstname": "unit", "lastname": "test"}}');
-
+        if (version_compare(PHP_VERSION, '5.5.0', '<'))
+        {
+            $stub = $this->getMockBuilder('CurlClient')
+                         ->setMethods(array('httpRequest'))
+                         ->getMock();
+            $stub->method('httpRequest')
+                     ->willReturn(
+                         '{"code":0, "data":{"id":"1", "firstname": "unit", "lastname": "test"}}'
+                     );
+        }
+        else
+        {
+            $stub = $this->createMock(CurlClient::class);
+            $stub->method('httpRequest')
+                 ->willReturn(
+                     '{"code":0, "data":{"id":"1", "firstname": "unit", "lastname": "test"}}'
+                 );
+        }
 
         $client = new Ontraport("2_AppID_12345678","Key5678", $stub);
         $requestParams = array(
