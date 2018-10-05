c08149d5b74ceec41175bf4ef93c059a5c6ed3bc
Austin Keil <austin.c.keil@gmail.com>
2018-10-05 11:09:00 -0700

    added 3 tests to CriteriaTest.php

--- tests/CriteriaTest.php
+++ tests/CriteriaTest.php
@@ -8,11 +8,11 @@ use OntraportAPI\Criteria;
 class CriteriaTest extends TestCase
 {
 
-    public function testFromArray()
+    public function testBuildCondition()
     {
         $condition = new Criteria("name", "=", "name");
         $str_condition = var_export($condition, true);
-        $this->assertEquals($str_condition, 'OntraportAPI\Criteria::__set_state(array(
+        $this->assertEquals('OntraportAPI\Criteria::__set_state(array(
    \'_condition\' => 
   array (
     0 => 
@@ -28,7 +28,94 @@ class CriteriaTest extends TestCase
       ),
     ),
   ),
-))');
-
+))', $str_condition);
     }
+
+    public function testAndCondition()
+    {
+        $condition = new Criteria("name", "=", "name");
+        $condition->andCondition(1, "<", 3);
+        $str_condition = var_export($condition, true);
+        $this->assertEquals('OntraportAPI\Criteria::__set_state(array(
+   \'_condition\' => 
+  array (
+    0 => 
+    array (
+      \'field\' => 
+      array (
+        \'field\' => \'name\',
+      ),
+      \'op\' => \'=\',
+      \'value\' => 
+      array (
+        \'value\' => \'name\',
+      ),
+    ),
+    1 => \'AND\',
+    2 => 
+    array (
+      \'field\' => 
+      array (
+        \'field\' => 1,
+      ),
+      \'op\' => \'<\',
+      \'value\' => 
+      array (
+        \'value\' => 3,
+      ),
+    ),
+  ),
+))',$str_condition);
+    }
+
+    public function testOrCondition()
+    {
+        $condition = new Criteria("name", "=", "name");
+        $condition->orCondition(1, "<", 3);
+        $str_condition = var_export($condition, true);
+        $this->assertEquals('OntraportAPI\Criteria::__set_state(array(
+   \'_condition\' => 
+  array (
+    0 => 
+    array (
+      \'field\' => 
+      array (
+        \'field\' => \'name\',
+      ),
+      \'op\' => \'=\',
+      \'value\' => 
+      array (
+        \'value\' => \'name\',
+      ),
+    ),
+    1 => \'OR\',
+    2 => 
+    array (
+      \'field\' => 
+      array (
+        \'field\' => 1,
+      ),
+      \'op\' => \'<\',
+      \'value\' => 
+      array (
+        \'value\' => 3,
+      ),
+    ),
+  ),
+))',$str_condition);
+    }
+
+    public function testFromArray()
+    {
+        $condition = new Criteria("name", "=", "name");
+        $str_condition = $condition->fromArray("[{
+\"field\":{\"field\":\"email\"},
+\"op\":\"=\",
+\"value\":{\"value\":\"test@test.com\"}
+}]");
+        $this->assertEquals('[{"field":{"field":"name"},"op":"=","value":{"value":"name"}}]', $str_condition);
+    }
+
+
+
 }
