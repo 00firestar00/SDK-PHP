7c27f609b0b8f7cec17c1b44d41c6c6492ec573f
Austin Keil <austin.c.keil@gmail.com>
2018-10-08 10:26:37 -0700

    added tests to ObjectFieldTest.php

--- tests/ObjectFieldTest.php
+++ tests/ObjectFieldTest.php
@@ -9,29 +9,39 @@ use OntraportAPI\Models\FieldEditor\ObjectSection;
 
 class ObjectFieldTest extends TestCase
 {
-    function testCreateFields()
+    function testToRequestParams()
     {
-        $mock_curl = new MockCurlClient();
-        $client = new Ontraport("2_AppID_12345678","Key5678", $mock_curl);
-
         $myField = new ObjectField("My New Field", ObjectField::TYPE_TEXT);
-
         $mySection = new ObjectSection("Contact Information", array($myField));
 
         $requestParams = $mySection->toRequestParams();
-        $requestParams["objectID"] = ObjectType::CONTACT;
-        $response = $client->object()->createFields($requestParams);
-        $this->assertEquals('{
-  "code": 0,
-  "data": {
-    "success": {
-      "f1557": "string"
-    },
-    "error": []
-  },
-  "account_id": 50
-}' , $response);
+        $this->assertEquals('{"name":"Contact Information","description":null,"fields":[[{"alias":"My New Field","required":0,"unique":0,"type":"text"}]]}' , json_encode($requestParams));
     }
+
+    function testSetAndGetID()
+    {
+        $myField = new ObjectField("My New Field", ObjectField::TYPE_TEXT);
+        $myField->setId(3);
+        $myFieldID = $myField->getId();
+        $this->assertEquals(3, $myFieldID);
+    }
+
+    function testSetAndGetFields()
+    {
+        $myField = new ObjectField("My New Field", ObjectField::TYPE_TEXT);
+        $myField->setField("My Newer Field");
+        $myNewerField = $myField->getField();
+        $this->assertEquals('My Newer Field', $myNewerField);
+    }
+
+    function testSetAndGetAlias()
+    {
+        $myField = new ObjectField("My New Field", ObjectField::TYPE_TEXT);
+        $myField->setAlias("My New Alias");
+        $myNewAlias = $myField->getAlias();
+        $this->assertEquals('My New Alias', $myNewAlias);
+    }
+
 
     function testAddThenRemoveOptions()
     {
@@ -86,7 +96,7 @@ class ObjectFieldTest extends TestCase
   "account_id": 50
 }' , $response);
     }
-
+    
     /**
      * @expectedException  \OntraportAPI\Exceptions\FieldTypeException
      */
