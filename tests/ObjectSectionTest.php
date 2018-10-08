463548682e71636c49f4b76c7cb0aa4245f95f76
Austin Keil <austin.c.keil@gmail.com>
2018-10-08 11:23:46 -0700

    added getFieldByAlias() to ObjectSectionTest.php

--- tests/ObjectSectionTest.php
+++ tests/ObjectSectionTest.php
@@ -16,4 +16,14 @@ class ObjectSectionTest extends TestCase
         $requestParams = $mySection->toRequestParams();
         $this->assertEquals('{"name":"Contact Information","description":null,"fields":[[{"alias":"My New Field","required":0,"unique":0,"type":"text"},{"alias":"My New Dropdown","required":0,"unique":0,"type":"drop"}]]}', json_encode($requestParams));
     }
+
+    function testGetFieldByAlias()
+    {
+        $myField = new ObjectField("My New Field", ObjectField::TYPE_TEXT);
+        $myDropDown = new ObjectField("My New Dropdown", ObjectField::TYPE_DROP);
+        $mySection = new ObjectSection("Contact Information", array($myField, $myDropDown));
+        $myFieldByAlias = $mySection->getFieldByAlias("My New Dropdown");
+        $stringMyFieldByAlias = json_encode(json_decode($myFieldByAlias));
+        $this->assertEquals('{"alias":"My New Dropdown","required":0,"unique":0,"type":"drop"}', $stringMyFieldByAlias);
+    }
 }
